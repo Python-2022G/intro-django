dc7d2f924a8ff12679e6ad0675506a492816e952
djumanov <diyorbekjumanov1120@gmail.com>
2023-07-04 15:39:23 +0500

    home.html

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
 
-from api.views import home, about, contact, say_hi, get_sum
+from api.views import home
 
 
 
 urlpatterns = [
     path('home/', home),
-    path('about/', about),
-    path('contact/', contact),
-    path('hi/<first>/<last>', say_hi),
-    path('sum/', get_sum),
 ]
--- api/views.py
+++ api/views.py
@@ -1,41 +1,6 @@
-from django.http import HttpRequest, HttpResponse, JsonResponse
-import json
+from django.http import HttpRequest
+from django.shortcuts import render
 
-def home(request: HttpRequest) -> HttpResponse:
+def home(request: HttpRequest):
 
-    return HttpResponse('index page')
-
-
-def about(request: HttpRequest) -> HttpResponse:
-    
-    return HttpResponse('about page')
-
-
-def contact(request: HttpRequest) -> HttpResponse:
-    
-    return HttpResponse('contact page')
-
-
-def say_hi(request: HttpRequest, first: str, last: str) -> HttpResponse:
-
-    return HttpResponse(f'<h1>Hi, {first} {last}</h1>')
-
-
-def get_sum(request: HttpRequest) -> JsonResponse:
-    if request.method == 'GET':
-        # get query params from reqeust
-        parmas = request.GET
-
-        a = parmas.get('a', 0)
-        b = parmas.get('b', 0)
-    
-    elif request.method == 'POST':
-        # get data from request
-        data_json = request.body.decode('utf-8')
-        # convert to dict
-        data = json.loads(data_json)
-
-        a = data.get('a', 0)
-        b = data.get('b', 0)
-
-    return JsonResponse({'sum': int(a) + int(b)})
+    return render(request, 'home.html')
